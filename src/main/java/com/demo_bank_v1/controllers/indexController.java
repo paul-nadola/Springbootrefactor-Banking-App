4f66628e55e37dc485a6712c859969e31ccdb4bc
Paul Nadola Oyinga <nadolapaul@gmail.com>
2024-02-13 16:44:54 +0300

    added login and register endpoints

--- src/main/java/com/demo_bank_v1/controllers/indexController.java
+++ src/main/java/com/demo_bank_v1/controllers/indexController.java
@@ -16,4 +16,20 @@ public class indexController {
         System.out.println("In Index Controller");
         return getIndexPage;
     }
+
+    @GetMapping("/login")
+    public ModelAndView getLogin(){
+        ModelAndView getLoginPage = new ModelAndView("login");
+        System.out.println("In login page controller");
+        getLoginPage.addObject("PageTitle", "Login");
+        return getLoginPage;
+    }
+
+    @GetMapping("/register")
+    public ModelAndView getRegister(){
+        ModelAndView getRegisterPage = new ModelAndView("register");
+        System.out.println("In register page controller");
+        getRegisterPage.addObject("PageTitle", "Register");
+        return getRegisterPage;
+    }
 }
